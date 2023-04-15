31ade5befe93ae746553404575bb272780565d76
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-15 22:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateClock() {
+			const now = new Date();
+			const date = now.toLocaleDateString();
+			const time = now.toLocaleTimeString();
+			document.getElementById('timer').innerHTML = `${date} ${time}`;
+		}
+
+		// call the function every second to update the time
+		setInterval(updateClock, 1000);
